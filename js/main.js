55bd20434fc8b89f528472e9334d4f774598f818
Abby <amhoschouer@wpi.edu>
2023-05-16 18:35:02 -0400

    add and remove are saved

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 let id = 0;
 let app = new Vue({
         el: "#vueApp",
-
         data: {
             welcomeMessage: 'Hello World!',
             titleToDo: 'To Do List',
@@ -9,21 +8,29 @@ let app = new Vue({
             newTodo: '',
             todos: []
         },
+
         methods: {
 
             addTodo() { //adds new task to todo list
                 this.todos.push({id: id++, text: this.newTodo, done: false})
                 this.newTodo = ''
+                localStorage.setItem('todos', JSON.stringify(this.todos))
             },
 
             removeTodo(todo) { //removes a task from a todo list
                 this.todos = this.todos.filter((x) => x !== todo)
+                localStorage.setItem('todos', JSON.stringify(this.todos))
+            },
+
+            finishTodo() {
+
             }
 
         },
 
         mounted() {
 
+                this.todos = JSON.parse(localStorage.getItem('todos')) || [];
         },
 
     });
