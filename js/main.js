e64376a39e0b174e20939474d18edb40f5400609
Abby <amhoschouer@wpi.edu>
2023-05-18 11:13:09 -0400

    fixed id to be inside of app

--- js/main.js
+++ js/main.js
@@ -1,38 +1,36 @@
-let id = 0;
 let app = new Vue({
-        el: "#vueApp",
-        data: {
-            welcomeMessage: 'Hello World!',
-            titleToDo: 'To Do List',
-            placeHolder: 'Enter a task here',
-            newTodo: '',
-            todos: []
+    el: "#vueApp",
+    data: {
+        welcomeMessage: 'Hello World!',
+        titleToDo: 'To Do List',
+        placeHolder: 'Enter a task here',
+        newTodo: '',
+        id: 0,
+        todos: []
+    },
+    methods: {
+        addTodo() { //adds new task to todo list
+            this.todos.push({id: this.id++, text: this.newTodo, done: false})
+            this.newTodo = ''
         },
 
-        methods: {
-
-            addTodo() { //adds new task to todo list
-                this.todos.push({id: id++, text: this.newTodo, done: false})
-                this.newTodo = ''
-            },
-
-            removeTodo(todo) { //removes a task from a todo list
-                this.todos = this.todos.filter((x) => x !== todo)
-            },
-
+        removeTodo(todo) { //removes a task from a todo list
+            this.todos = this.todos.filter((x) => x !== todo)
         },
 
-        mounted() {
-                this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-        },
+    },
+
+    mounted() {
+        this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+    },
 
     watch: {
         todos: {
             deep: true,
-            handler(){
+            handler() {
                 localStorage.setItem('todos', JSON.stringify(this.todos))
             }
         }
     }
 
-    });
+});
